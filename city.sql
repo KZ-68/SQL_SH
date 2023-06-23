17ed368ed6d4e72b2ba931c59ae416b24a7faeb2
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-23 19:45:02 +0200

    feat: add last queries and finished

--- city.sql
+++ city.sql
@@ -57,7 +57,38 @@ LIMIT 10
 -- 7. Compter le nombre de villes dont le nom commence par “Saint”
 SELECT 
 	COUNT(ville_nom) AS nbr_ville
-FROM `villes_france_free` 
+FROM villes_france_free 
 WHERE ville_nom LIKE 'Saint%'
 
 -- 8. Obtenir la liste des villes qui ont un nom existants plusieurs fois, et trier afin d’obtenir en premier celles dont le nom est le plus souvent utilisé par plusieurs communes
+SELECT 
+	ville_nom, 
+	COUNT(*) AS nbr_occurence 
+FROM villes_france_free 
+GROUP BY `ville_nom` 
+ORDER BY nbr_occurence DESC
+
+-- 9. Obtenir en une seule requête SQL la liste des villes dont la superficie est supérieur à la superficie moyenne
+SELECT 
+	ville_nom,
+	ville_surface
+FROM villes_france_free 
+WHERE ville_surface > (
+	SELECT 
+		AVG(ville_surface) 
+	FROM villes_france_free
+	)
+
+-- 10. Obtenir la liste des départements qui possèdent plus de 2 millions d’habitants
+SELECT 
+	ville_departement, 
+	SUM(ville_population_2012) AS populations
+FROM villes_france_free 
+GROUP BY ville_departement
+HAVING populations > 2000000
+ORDER BY populations DESC
+
+-- 11. Remplacez les tirets par un espace vide, pour toutes les villes commençant par “SAINT-” (dans la colonne qui contient les noms en majuscule)
+UPDATE villes_france_free
+SET ville_nom = REPLACE(ville_nom, '-', ' ') 
+WHERE ville_nom LIKE 'SAINT-%'
